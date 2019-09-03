59698f88baffa48b85bbf8a557c08c8e4ac7de95
Zhao Yang <zhaoyang@ZhaodeMac-mini.local>
2019-09-03 11:43:48 +0800

    修复定义和pod

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
 declare module '@yz1311/react-native-http-cache' {
     export function clearCache(): Promise<void>;
 
-    export function getHttpCacheSize(): Promise<void>;
+    export function getHttpCacheSize(): Promise<number>;
 
-    export function getImageCacheSize(): Promise<void>;
+    export function getImageCacheSize(): Promise<number>;
 
     export function clearImageCache(): Promise<void>;
+
+    export function getCacheSize(): Promise<number>;
+
+    export function clearCache(): Promise<void>;
 }
